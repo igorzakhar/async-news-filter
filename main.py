81abb5918335b2ef47f2b9101b87a2281806c0b8
zakharov <igorzakhar@gmail.com>
2019-10-12 12:34:13 +0300

    add dictionaries of charged words

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import aiohttp
 import asyncio
+from os import listdir
+from os.path import isfile, join
 from urllib.parse import urlparse
 
 import adapters
+import aiofiles
 import pymorphy2
 from text_tools import split_by_words, calculate_jaundice_rate
 
 
 def extract_domain_name(url):
     return '_'.join(urlparse(url).netloc.split('.'))
+
+
+async def get_charged_words(path):
+    words = []
+    files = [join(path, fn) for fn in listdir(path) if isfile(join(path, fn))]
+    for file in files:
+        async with aiofiles.open(file, mode='r') as afp:
+            contents = await afp.read()
+            words.extend(contents.strip().split('\n'))
+    return words
 
 
 async def fetch(session, url):
@@ -19,8 +32,11 @@ async def fetch(session, url):
 
 async def main():
 
+    path = 'charged_dict'
+    charged_words = await get_charged_words(path)
+
     morph = pymorphy2.MorphAnalyzer()
-    charged_words = ['звезда', 'прогресс', 'комета', 'астероид']
+
     url = 'https://inosmi.ru/science/20191011/246010355.html'
 
     async with aiohttp.ClientSession() as session:
