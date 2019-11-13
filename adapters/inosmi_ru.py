3c44199a5df71dc08e4de0b59df78e8e2539013f
zakharov <igorzakhar@gmail.com>
2019-11-13 11:30:37 +0300

    Move adapters tests to 'test' directory

--- adapters/inosmi_ru.py
+++ adapters/inosmi_ru.py
@@ -8,12 +8,12 @@ from .html_tools import remove_buzz_attrs, remove_buzz_tags, remove_all_tags
 
 def sanitize(html, plaintext=False):
     soup = BeautifulSoup(html, 'html.parser')
-    title = soup.select_one('h1.article-header__title').get_text()
     articles = soup.select('article.article')
 
     if len(articles) != 1:
         raise ArticleNotFound()
 
+    title = soup.select_one('h1.article-header__title').get_text()
     article = articles[0]
     article.attrs = {}
 
@@ -35,36 +35,3 @@ def sanitize(html, plaintext=False):
         text = article.get_text()
 
     return text.strip(), title
-
-
-def test_sanitize():
-    resp = requests.get('https://inosmi.ru/economic/20190629/245384784.html')
-    resp.raise_for_status()
-    clean_text = sanitize(resp.text)
-
-    assert clean_text.startswith('<article>')
-    assert clean_text.endswith('</article>')
-    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
-    assert 'За несколько часов до встречи с Си' in clean_text
-
-    assert '<img src="' in clean_text
-    assert '<a href="' in clean_text
-    assert '<h1>' in clean_text
-
-    clean_plaintext = sanitize(resp.text, plaintext=True)
-
-    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_plaintext
-    assert 'За несколько часов до встречи с Си' in clean_plaintext
-
-    assert '<img src="' not in clean_plaintext
-    assert '<a href="' not in clean_plaintext
-    assert '<h1>' not in clean_plaintext
-    assert '</article>' not in clean_plaintext
-    assert '<h1>' not in clean_plaintext
-
-
-def test_sanitize_wrong_url():
-    resp = requests.get('http://example.com')
-    resp.raise_for_status()
-    with pytest.raises(ArticleNotFound):
-        sanitize(resp.text)
---
+++ tests/test_inosmi_sanitize.py
@@ -0,0 +1,39 @@
+import pytest
+import requests
+
+from adapters import ArticleNotFound, SANITIZERS
+
+sanitize = SANITIZERS.get('inosmi_ru')
+
+
+def test_sanitize():
+    resp = requests.get('https://inosmi.ru/economic/20190629/245384784.html')
+    resp.raise_for_status()
+    clean_text, _ = sanitize(resp.text)
+
+    assert clean_text.startswith('<article>')
+    assert clean_text.endswith('</article>')
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
+    assert 'За несколько часов до встречи с Си' in clean_text
+
+    assert '<img src="' in clean_text
+    assert '<a href="' in clean_text
+    assert '<h1>' in clean_text
+
+    clean_plaintext, _ = sanitize(resp.text, plaintext=True)
+
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_plaintext
+    assert 'За несколько часов до встречи с Си' in clean_plaintext
+
+    assert '<img src="' not in clean_plaintext
+    assert '<a href="' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+    assert '</article>' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+
+
+def test_sanitize_wrong_url():
+    resp = requests.get('http://example.com')
+    resp.raise_for_status()
+    with pytest.raises(ArticleNotFound):
+        sanitize(resp.text)
